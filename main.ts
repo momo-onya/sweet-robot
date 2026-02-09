7538c5a03906ccb6477c9b5a6bfac83997367e7d
momo-onya <tm.oraya1509@gmail.com>
2026-02-09 12:36:01 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -10,3 +10,15 @@
         //% block="D"
         D = 3,
     }
+//% block="motor left %left speed %leftspeed right %right speed %rightspeed"
+//% leftspeed.min=-100 leftspeed.max=100
+//% rightspeed.min=-100 rightspeed.max=100
+export function MotorPair(
+    left: MotorPort,
+    leftspeed: Number,
+    right: MotorPort,
+    rightspeed: Number
+): void {
+    motorRun(left,leftspeed)
+    motorRun(right,rightspeed)
+}
