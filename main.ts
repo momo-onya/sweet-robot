f965599f632c68d0d1fa3407b2a30b81d05dea2b
momo-onya <tm.oraya1509@gmail.com>
2026-02-09 12:25:55 +0700

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-
+//% color="#FADCDC" icon="car"
+์namespace SweetieRobot {
+    export enum MotorPort {
+        //% block="A"
+        A = 0,
+        //% block="B"
+        B = 1,
+        //% block="C"
+        C = 2,
+        //% block="D"
+        D = 3,
+    }
